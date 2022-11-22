e005578e9e311188f6de9a6050dc595a042f6118
Ngoc Duxng <dungbachngoc20@gmail.com>
2022-11-22 21:07:00 +0700

    change url login

--- src/apis/modules/auth.js
+++ src/apis/modules/auth.js
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
 import Api from '../../apis';
 import request from "axios";
+// https://localhost:5001
 
 export default {
     login(credentials) {
         // console.log(Api());
-        return request.post(`https://localhost:5001/api/Auth/login`, credentials)
+        return request.post(`https://ndzttt.com/api/Auth/login`, credentials)
                 .then((response) => {
                     // console.log(response);
                     return response;
